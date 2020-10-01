ceffab6fd49f8ff3532c26f369d9a9920e6a9438
Dharma Sarswat <dharmasarswat001@gmail.com>
2020-10-02 02:29:05 +0530

    Updates

---
+++ precache-manifest.d791952088a10da5e79815fde8078a3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7162e0c0c498d98846c93684ee05871",
+    "url": "/drawCanvas/index.html"
+  },
+  {
+    "revision": "dac8787818db5130f0bc",
+    "url": "/drawCanvas/static/css/main.a15f8c71.chunk.css"
+  },
+  {
+    "revision": "2a46d6f8a656794e63fb",
+    "url": "/drawCanvas/static/js/2.0acb832f.chunk.js"
+  },
+  {
+    "revision": "bb63a167a861814d9658",
+    "url": "/drawCanvas/static/js/3.21a506f8.chunk.js"
+  },
+  {
+    "revision": "9cbc51c3f20ff9e60db67bbc6e4216b8",
+    "url": "/drawCanvas/static/js/3.21a506f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d1b42c32c562bb96d9",
+    "url": "/drawCanvas/static/js/4.bcf4e40b.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/drawCanvas/static/js/4.bcf4e40b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d96435f1ee703576c8f",
+    "url": "/drawCanvas/static/js/5.2321baa1.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/drawCanvas/static/js/5.2321baa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac8787818db5130f0bc",
+    "url": "/drawCanvas/static/js/main.f395ca3f.chunk.js"
+  },
+  {
+    "revision": "53e6e7dac317192a26bf",
+    "url": "/drawCanvas/static/js/runtime-main.2c863df3.js"
+  }
+]);
